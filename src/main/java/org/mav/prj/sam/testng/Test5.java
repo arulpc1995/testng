232eddd74c928039148c44806177f6e84d871aa8
teamlead11 <automationlead11@gmail.com>
2019-03-29 08:59:15 +0530

    added test class 5

---
+++ src/main/java/org/mav/prj/sam/testng/Test5.java
@@ -0,0 +1,10 @@
+package org.mav.prj.sam.testng;
+
+public class Test5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is my  test 5");
+	}
+
+}
