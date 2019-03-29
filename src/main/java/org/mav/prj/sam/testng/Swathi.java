5d211e4d77cc3a913a02b2d52e4bd7df51ebe9c5
SwathiGumma <swathi.gumma21@gmail.com>
2019-03-29 16:19:17 +0530

    added swathi

---
+++ src/main/java/org/mav/prj/sam/testng/Swathi.java
@@ -0,0 +1,14 @@
+package org.mav.prj.sam.testng;
+
+public class Swathi {
+public void name() {
+	System.out.println("swathi");
+
+}
+	public static void main(String[] args) {
+		Swathi swa=new Swathi();
+		swa.name();
+
+	}
+
+}
