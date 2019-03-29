347e2548b59cfb32a533e23732f02b5bec30c635
technosoft14 <technosoft1408@gmail.com>
2019-03-29 11:03:33 +0530

    Methodpgm

--- src/main/java/org/mav/prj/sam/testng/App.java
+++ src/main/java/org/mav/prj/sam/testng/App.java
@@ -9,5 +9,11 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        
     }
+    private void Arraysortpoorna() {
+		// TODO Auto-generated method stub
+    	System.out.println("India");
+
+	}
 }
