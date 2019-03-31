aed4cef8bfd7158f0e513bd7255da830c11f1fea
LeSoleil11 <lavan.invictus@gmail.com>
2019-03-31 21:47:32 +0530

    Yours Faithfully - Accept pull request

---
+++ src/main/java/org/mav/prj/sam/testng/lavnPlay.java
@@ -0,0 +1,11 @@
+package org.mav.prj.sam.testng;
+
+public class lavnPlay {
+
+	public static void main(String[] args) {
+
+		if (args.length < 1) {
+			System.out.println("Plz text during runtime");
+		}
+	}
+}
