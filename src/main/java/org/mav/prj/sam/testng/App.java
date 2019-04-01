95b4b1f6a375cb39c6fa66fb653b64d14963c165
teamlead11 <automationlead11@gmail.com>
2019-04-01 09:01:16 +0530

    demo 2 by user2

--- src/main/java/org/mav/prj/sam/testng/App.java
+++ src/main/java/org/mav/prj/sam/testng/App.java
@@ -4,14 +4,17 @@ package org.mav.prj.sam.testng;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
-    private void arrysortbyBasavaiah() {
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+	private void arrysortbyBasavaiah() {
 		System.out.println("Welcome");
 
+	}
+
+	public static void add(int a, int b, int c) {
+		System.out.println(a + b + c);
 	}
 }
