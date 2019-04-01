b3ec37b578ee886168e158f2fc0bcd89f06530ea
teamlead11 <automationlead11@gmail.com>
2019-04-01 08:27:24 +0530

    added demo class

---
+++ src/main/java/org/mav/prj/sam/testng/Demo.java
@@ -0,0 +1,10 @@
+package org.mav.prj.sam.testng;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("some code");
+	}
+
+}
