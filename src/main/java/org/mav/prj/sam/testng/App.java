cca30627624d7b0ca1c50583b8f16bf4b4e6b14f
arulpc1995 <arulpc1995@gmail.com>
2019-04-01 09:43:10 +0530

    Demo new

--- src/main/java/org/mav/prj/sam/testng/App.java
+++ src/main/java/org/mav/prj/sam/testng/App.java
@@ -10,4 +10,8 @@ public class App
     {
         System.out.println( "Hello World!" );
     }
+    public void ArraysortKkArul() {
+		// TODO Auto-generated method stub
+
+	}
 }
---
+++ src/main/java/org/mav/prj/sam/testng/Demonew.java
@@ -0,0 +1,11 @@
+package org.mav.prj.sam.testng;
+
+public class Demonew {
+
+	public void arulsort(int a,int b) {
+		System.out.println(a+b);
+
+	}
+	
+	
+}
