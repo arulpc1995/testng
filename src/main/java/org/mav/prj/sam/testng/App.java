24228f37170014b6c25bb23a3888729436d0ccad
teamlead11 <automationlead11@gmail.com>
2019-04-01 08:35:16 +0530

    demo comit by user1

--- src/main/java/org/mav/prj/sam/testng/App.java
+++ src/main/java/org/mav/prj/sam/testng/App.java
@@ -4,10 +4,12 @@ package org.mav.prj.sam.testng;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+	public static void arraySort() {
+		System.out.println("code by user1");
+	}
 }
